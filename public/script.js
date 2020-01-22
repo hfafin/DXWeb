d16ed8ca13f283b891e2061b50be8d23e8bde3a7
hfafin <58565156+hfafin@users.noreply.github.com>
2020-01-22 17:40:25 +0100

    Add files via upload

--- public/script.js
+++ public/script.js
@@ -1,19 +1,103 @@
 import resumeContext from './resumeContext.js';
 const $btn = document.querySelector('button');
 
+let resumed = false;
+
+var ctx = new AudioContext();
+		var out = ctx.destination;
+		var envA = ctx.createGain();
+		var envB = ctx.createGain();
+
+		const $frequencyA = document.createElement('input');
+	 		$frequencyA.type = 'range';
+	  		$frequencyA.id = 'Frequency';
+	  		$frequencyA.name = 'Frequency';
+	  		$frequencyA.min = 20;
+	  		$frequencyA.max = 1000;
+	  		$frequencyA.step = 1;
+	  		$frequencyA.value = 100;
+	  		$frequencyA.addEventListener('input', e => {
+	    	const frequencyA = parseFloat(e.target.value);
+	    	OscA.frequency.value = frequencyA;
+	  	});
+	  	document.body.appendChild($frequencyA);
+
+		const $gain = document.createElement('input');
+			$gain.type = 'range';
+			$gain.id = 'Gain';
+		  	$gain.name = 'Gain';
+			$gain.min = 0;
+			$gain.max = 1;
+			$gain.step = 0.01;
+			$gain.value = 0;
+			$gain.addEventListener('input', e => {
+				const vol = parseFloat(e.target.value);
+				envA.gain.value = vol;
+			});
+			document.body.appendChild($gain);
+
+		const $mod = document.createElement('input');
+			$mod.type = 'range';
+			$mod.id = 'ModulationAmount';
+		  	$mod.name = 'ModulationAmount';
+			$mod.min = 0;
+			$mod.max = 100;
+			$mod.step = 0.01;
+			$mod.value = 0;
+			$mod.addEventListener('input', e => {
+				const mod = parseFloat(e.target.value);
+				envB.gain.value = mod;
+			})
+
+
+			document.body.appendChild($mod);
+
+		const $frequencyB = document.createElement('input');
+			$frequencyB.type = 'range';
+			$frequencyB.id = 'ModulationFrequency';
+		  	$frequencyB.name = 'ModulationFrequency';
+			$frequencyB.min = 20;
+			$frequencyB.max = 500;
+			$frequencyB.step = 0.1;
+			$frequencyB.value = 20;
+			$frequencyB.addEventListener('input', e => {
+				const fB = parseFloat(e.target.value);
+				OscB.frequency.value = fB;
+			});
+			document.body.appendChild($frequencyB);
+
+		var labelFrequency = document.createElement('label');
+			labelFrequency.htmlFor = 'Frequency';
+			labelFrequency.innerHTML = 'Frequency  ';
+		document.body.appendChild(labelFrequency);
+
+		var labelGain = document.createElement('label');
+			labelGain.htmlFor = 'Volume';
+			labelGain.innerHTML = 'Volume  ';
+		document.body.appendChild(labelGain);
+
+		var labelMod = document.createElement('label');
+			labelMod.htmlFor = 'ModulationAmount';
+			labelMod.innerHTML = 'Modulation Amount  ';
+		document.body.appendChild(labelMod);
+
+		var labelModFreq = document.createElement('label');
+			labelModFreq.htmlFor = 'ModulationFrequency';
+			labelModFreq.innerHTML = 'Modulation Frequency ';
+		document.body.appendChild(labelModFreq);
+
+
 
 async function init()
 {
 	let ratio = 1;
 
-	var ctx = new AudioContext();
-	var out = ctx.destination;
-	var envA = ctx.createGain();
-	var envB = ctx.createGain();
-
-
-	await resumeContext(ctx);
-
+	if(!resumed){
+		await resumeContext(ctx);
+		resumed = true;
+	}
+	const now = ctx.currentTime;
+	
 	envA.gain.value = 0;
 	envB.gain.value = 0;
 
@@ -21,87 +105,6 @@ async function init()
 	var OscB = ctx.createOscillator();
 	/*var OscC = ctx.createOscillator();
 	var OscD = ctx.createOscillator();*/
-
-
-	const $frequencyA = document.createElement('input');
-	  $frequencyA.type = 'range';
-	  $frequencyA.id = 'Frequency';
-	  $frequencyA.name = 'Frequency';
-	  $frequencyA.min = 20;
-	  $frequencyA.max = 1000;
-	  $frequencyA.step = 1;
-	  $frequencyA.value = 100;
-	  $frequencyA.addEventListener('input', e => {
-	    const frequencyA = parseFloat(e.target.value);
-	    OscA.frequency.value = frequencyA;
-	  });
-	  document.body.appendChild($frequencyA);
-
-	const $gain = document.createElement('input');
-		$gain.type = 'range';
-		$gain.id = 'Gain';
-	  	$gain.name = 'Gain';
-		$gain.min = 0;
-		$gain.max = 1;
-		$gain.step = 0.01;
-		$gain.value = 0;
-		$gain.addEventListener('input', e => {
-			const vol = parseFloat(e.target.value);
-			envA.gain.value = vol;
-		});
-		document.body.appendChild($gain);
-
-	const $mod = document.createElement('input');
-		$mod.type = 'range';
-		$mod.id = 'ModulationAmount';
-	  	$mod.name = 'ModulationAmount';
-		$mod.min = 0;
-		$mod.max = 100;
-		$mod.step = 0.01;
-		$mod.value = 0;
-		$mod.addEventListener('input', e => {
-			const mod = parseFloat(e.target.value);
-			envB.gain.value = mod;
-		})
-
-
-		document.body.appendChild($mod);
-
-	const $frequencyB = document.createElement('input');
-		$frequencyB.type = 'range';
-		$frequencyB.id = 'ModulationFrequency';
-	  	$frequencyB.name = 'ModulationFrequency';
-		$frequencyB.min = 20;
-		$frequencyB.max = 500;
-		$frequencyB.step = 0.1;
-		$frequencyB.value = 20;
-		$frequencyB.addEventListener('input', e => {
-			const fB = parseFloat(e.target.value);
-			OscB.frequency.value = fB;
-		});
-		document.body.appendChild($frequencyB);
-
-	var labelFrequency = document.createElement('label');
-		labelFrequency.htmlFor = 'Frequency';
-		labelFrequency.innerHTML = 'Frequency  ';
-	document.body.appendChild(labelFrequency);
-
-	var labelGain = document.createElement('label');
-		labelGain.htmlFor = 'Volume';
-		labelGain.innerHTML = 'Volume  ';
-	document.body.appendChild(labelGain);
-
-	var labelMod = document.createElement('label');
-		labelMod.htmlFor = 'ModulationAmount';
-		labelMod.innerHTML = 'Modulation Amount  ';
-	document.body.appendChild(labelMod);
-
-	var labelModFreq = document.createElement('label');
-		labelModFreq.htmlFor = 'ModulationFrequency';
-		labelModFreq.innerHTML = 'Modulation Frequency ';
-	document.body.appendChild(labelModFreq);
-
-
 
 	OscA.start();
 	OscB.start();
@@ -120,4 +123,4 @@ async function init()
 
 window.addEventListener('load', init);
 
-$btn.addEventListener('click',init);
+$btn.addEventListener('click', init);
